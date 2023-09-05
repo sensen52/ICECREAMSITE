912c3cf5aa1705e66f03d68d93d1b781f1852039
kshkshksh <ksh931101@naver.com>
2023-09-05 22:41:56 +0900

    0905

--- src/main/resources/static/js/mypage.js
+++ src/main/resources/static/js/mypage.js
@@ -1,8 +1,25 @@
-// 이동하기 axios
+const settingsItems = document.querySelectorAll(".settingshref");
+  const iceOptions = document.querySelectorAll(".ice_options");
+  const closeItems2 = document.querySelectorAll(".close2");
 
-//회원정보수정으로
+  settingsItems.forEach((settingsItem, index) => {
+    settingsItem.addEventListener("click", () => {
+      toggleSettingsPage(index); // 해당 버튼을 클릭하면 해당 인덱스에 해당하는 .ice_options 요소를 토글
+    });
+  });
+
+  closeItems2.forEach((closeItem, index) => {
+    closeItem.addEventListener("click", () => {
+      toggleSettingsPage(index); // 해당 닫기 버튼을 클릭하면 해당 인덱스에 해당하는 .ice_options 요소를 토글
+    });
+  });
+
+  function toggleSettingsPage(index) {
+    iceOptions[index].classList.toggle("active"); // 해당 인덱스의 .ice_options 요소를 토글
+  }
+
+
 const updateBtn = document.getElementById('update-btn');
-
 
 updateBtn.addEventListener('click', () => {
     axios.get('http://localhost:8080/th/3ice/update')
@@ -14,15 +31,14 @@ updateBtn.addEventListener('click', () => {
         });
 });
 
-//회원탈퇴로 이동
 const deleteButton = document.getElementById('delete-btn');
-  deleteButton.addEventListener('click', () => {
-    const url = 'http://localhost:8080/th/3ice/withdraw';
-    axios.get(url)
-      .then(() => {
-        window.location.href = url;
-      })
-      .catch((error) => {
-        console.error('삭제 실패:', error);
-      });
+deleteButton.addEventListener('click', () => {
+const url = 'http://localhost:8080/th/3ice/withdraw';
+axios.get(url)
+  .then(() => {
+    window.location.href = url;
+  })
+  .catch((error) => {
+    console.error('요청 실패:', error);
   });
+});
